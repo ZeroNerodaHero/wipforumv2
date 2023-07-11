1628a326fb7a952ea310443606f1676f787b8ec3
root <root@test.test>
2023-07-10 23:30:26 -0700

    better solutouin

---
+++ backendFiles/login.php
@@ -0,0 +1,26 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');    
+    header('Access-Control-Allow-Headers:  *');
+    header('Access-Control-Allow-Methods:  *'); 
+    define ('SITE_ROOT', realpath(dirname(__FILE__)));
+
+    $servername = "db";
+    $database = "funpills";
+    $username = "root";
+    $password = "password";
+    $port = "3306";
+
+    $conn = new mysqli($servername, $username, $password,$database,$port);
+    if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
+    getcwd().DIRECTORY_SEPARATOR;
+    $post_image_dir = getcwd().DIRECTORY_SEPARATOR."/imgs/postImgs/";
+
+    function generateError($msg,$errorCode=0){
+        return '{"code":'.$errorCode.',"msg":"'.$msg.'"}';
+    }
+
+    $host_computer_loc = "https://funcel.xyz/request/imgs/postImgs/";
+    $host_computer_loc = "http://172.17.0.1:8070/imgs/postImgs/"; 
+
+?>
